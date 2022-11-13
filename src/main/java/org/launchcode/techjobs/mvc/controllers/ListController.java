d1cbe7822cae16832f7d783bfa802ab2fd9fa241
Joe Henke <jwhenke08@gmail.com>
2022-11-13 07:57:40 -0600

    GA3 commit

--- src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -17,16 +18,15 @@ import java.util.HashMap;
 @Controller
 @RequestMapping(value = "list")
 public class ListController {
-
     static HashMap<String, String> columnChoices = new HashMap<>();
     static HashMap<String, Object> tableChoices = new HashMap<>();
-
     public ListController () {
         columnChoices.put("all", "All");
         columnChoices.put("employer", "Employer");
         columnChoices.put("location", "Location");
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
+
 
         tableChoices.put("all",JobData.findAll());
         tableChoices.put("employer", JobData.getAllEmployers());
--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
-
 
 /**
  * Created by LaunchCode
@@ -19,13 +19,11 @@ import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoic
 @Controller
 @RequestMapping("search")
 public class SearchController {
-
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
-
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
